9fc1429ecfa75afb8d62242b3d9a6bf3e4f8c0b7
GowdaHemannth <hemanthagalahalli@gmail.com>
2026-01-19 11:04:24 +0530

    The New Files Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { InputBox } from './Componenst'
+
+// Then Here I Have Created   One Custommade Hook Right We can Use That Here
+import {CurrencyINFO} from './hooks'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[Amount,SetAmount]=useState(0)
+  const [From,SetFrom]=useState("usd")
+  const[To,SetTo]=useState("inr")
+  const[ConvertedAmount,SetConvertedAmount]=useState(0)
+  
+  const currencyInfo=useCurrency(From)
 
+  // Here I Will Be Doing That Once 
+  // all the keys here keys Means He optains
+  // if you Think We can Pas these Optiins to our sELECT bOX 
+  const Options= Object.keys(currencyInfo)
+
+  const Swap=()=>{
+    // here whatare Doing We Are Jsut wapping Value from here and Thier 
+    SetFrom(To)
+    SetTo(From) // That All 
+  }
+  // Then I Need Converted Amount Right 
+  const convert=()=>{
+    SetConvertedAmount(Amount*currencyInfo(To))
+  }
   return (
     <>
       <h1 className='bg-green-400'>Currency Converter</h1>
--- src/Componenst/Input.jsx
+++ src/Componenst/Input.jsx
@@ -1,54 +1,56 @@
-import  React from 'react'
-
+import React from "react";
 
 function InputBox({
-    label,
-    amount,
-    onAmountChange,
-    onCurrencyChange,
-    currencyOptions=[],
-    selectedCurrency='usd',                  // I Might Be Wondering what am i doing with Label and ClassNae 
-    amountDisable=false,
-    currencyDisable=false,                  // Its Nothing But the Props Which we Are Passing Through InputBox 
-                       // Here We will be Re using the Things 
-    className = "",
-}) {
-   
+  label,
+  amount,
+  onAmountChange,
 
-    return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex  ${className}`}>
-            <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
-                   {label} 
-                </label>
-                <input
-                    
-                    className="outline-none w-full bg-transparent py-1.5"
-                    type="number"
-                    placeholder="Amount"
-                    // Vlue that Should be Displayed on the Uer
-                    value={amount}
-                    onChange={(e)=> onAmountChange &&  onAmountChange(Number(e.target.value))}
-                />
-            </div>
-            <div className="w-1/2 flex flex-wrap justify-end text-right">
-                <p className="text-black/40 mb-2 w-full">Currency Type</p>
-                
-                <select
-                    className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    
-                >
-                    
-                        <option value="usd">
-                            usd
-                        </option>
-                
-                </select>
-            </div>
-        </div>
-    );
+  currencyOptions = [],
+  selectedCurrency = "usd",
+  onCurrencyChange, // I Might Be Wondering what am i doing with Label and ClassNae
+  amountDisable = false,
+  // Here We Might Be Having Doubt Why We Use These
+  // Think in Wesite and User Enters the Amount
+  // then Press Buttomn  then THe Inpuit Fies Becomes Disable
+  // So NOW HERE MIGHT BE THE QUESTION WHEN DOES IT WILL BECOME FLASE AGAIN
+  // SIMPLE LOGIC ADD ANOTHER EDIT BUTTION SO WHEN SOMEONE PREE EDIT BUTTON INPUT FIELS SHOULD BE
+  // ENABLED AGAIN
+  currencyDisable = false, // Its Nothing But the Props Which we Are Passing Through InputBox
+  // Here We will be Re using the Things
+  className = "",
+}) {
+  return (
+    <div className={`bg-white p-3 rounded-lg text-sm flex  ${className}`}>
+      <div className="w-1/2">
+        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <input
+          className="outline-none w-full bg-transparent py-1.5"
+          type="number"
+          placeholder="Amount"
+          // Vlue that Should be Displayed on the Uer
+          value={amount}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
+        />
+      </div>
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black/40 mb-2 w-full">Currency Type</p>
+
+        <select className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+        value={selectedCurrency}
+        // Here Currency Cahnges it Should be Updated to Current Selected INput 
+        // Hee we have Numeros Options From that Selected will bve Choosen
+        onChange={(e)=>onCurrencyChange(e.target.value)}
+        >
+         {currencyOptions.map((currency)=>(
+          // key used for performance 
+           <option key={currency} value={currency}>{currency}</option>
+         ))}
+        </select>
+      </div>
+    </div>
+  );
 }
 
 export default InputBox;
-
-
